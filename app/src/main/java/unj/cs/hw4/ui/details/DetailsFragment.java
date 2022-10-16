b08dca19134fa450d17df5e3b4d7af8e960ea1dd
Abim29 <cycryft156@gmail.com>
2022-10-16 19:06:40 +0700

    Add and Remove Coffee from Fav List

--- app/src/main/java/unj/cs/hw4/ui/details/DetailsFragment.java
+++ app/src/main/java/unj/cs/hw4/ui/details/DetailsFragment.java
@@ -1,28 +1,21 @@
 package unj.cs.hw4.ui.details;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import java.util.List;
-
-import unj.cs.hw4.MainActivity;
 import unj.cs.hw4.R;
 import unj.cs.hw4.adapter.FavoriteAdapter;
 import unj.cs.hw4.data.Datasource;
-import unj.cs.hw4.data.FavoriteCoffee;
 import unj.cs.hw4.databinding.FragmentDetailsBinding;
 import unj.cs.hw4.model.Coffee;
 
@@ -36,28 +29,13 @@ public class DetailsFragment extends Fragment {
         return binding;
     }
 
-    //    public DetailsFragment() {
-    //      Required empty public constructor
-    //    }
-
-//    public static DetailsFragment newInstance(String param1, String param2) {
-//        DetailsFragment fragment = new DetailsFragment();
-//        Bundle args = new Bundle();
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Bundle arguments = getArguments();
-        // Log.d("ARG", arguments.get(POSITION).toString());
         coffeeId = (int) arguments.get(POSITION);
         item = new Datasource().loadDataset().get(coffeeId);
-
-        // Log.d("ARG", String.valueOf(coffeeId));
-        // Log.d("COFFEE", item.toString());
         BottomNavigationView navBar = getActivity().findViewById(R.id.nav_view);
         navBar.setVisibility(View.GONE);
     }
@@ -70,8 +48,8 @@ public class DetailsFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentDetailsBinding.inflate(inflater, container, false);
         View root = getBinding().getRoot();
+        setIcon(getBinding().favoriteFab);
         return root;
-        // return inflater.inflate(R.layout.fragment_details, container, false);
     }
 
     @Override
@@ -87,15 +65,13 @@ public class DetailsFragment extends Fragment {
         fav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dataset.add(item)){
-                    Toast.makeText(getContext(), "Added to favorite!", Toast.LENGTH_SHORT).show();
+                if (!dataset.contains(item)) {
+                    addFavoriteCoffee(item);
+                } else {
+                    removeFavoriteCoffee(item);
                 }
-                // Log.d("FAV", String.valueOf(coffeeId));
-//                favCoffees.addFavoriteCoffee(item);
-//                Log.d("LIST FAV", favCoffees.getFavoriteCoffee().toString());
             }
         });
-        // title.setText(getString(R.string.coffee_name, "Americano"));
     }
 
     @Override
@@ -104,5 +80,35 @@ public class DetailsFragment extends Fragment {
         binding = null;
         BottomNavigationView navBar = getActivity().findViewById(R.id.nav_view);
         navBar.setVisibility(View.VISIBLE);
+    }
+
+    private void setIcon(FloatingActionButton floatingActionButton) {
+        if(floatingActionButton == null){
+            return;
+        }
+
+        if (!dataset.contains(item)) {
+            floatingActionButton.setImageResource(R.drawable.ic_favorite);
+        } else {
+            floatingActionButton.setImageResource(R.drawable.ic_delete);
+        }
+    }
+
+    private void addFavoriteCoffee(Coffee c) {
+        if(dataset.add(c)){
+            Toast.makeText(getContext(), "Added to favorite!", Toast.LENGTH_SHORT).show();
+            setIcon(getBinding().favoriteFab);
+        } else {
+            Toast.makeText(getContext(), "Failed to add to favorite!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void removeFavoriteCoffee(Coffee c) {
+        if(dataset.remove(c)){
+            Toast.makeText(getContext(), "Removed from favorite!", Toast.LENGTH_SHORT).show();
+            setIcon(getBinding().favoriteFab);
+        } else {
+            Toast.makeText(getContext(), "Failed to remove from favorite!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
